57516038e6b652e04f55cdfb598d2b0bb6c56be4
Mikheil-Abuladze <abuladzemikheil1995@gmail.com>
2025-02-14 22:19:03 +0400

    Finished work

---
+++ index.js
@@ -0,0 +1,14 @@
+const myname = "Mikheil";
+const dayoftheweek = "Friday";
+const favouriteactivity = " listening to music.";
+
+const sentence =
+  "Hi, my name is" +
+  " " +
+  myname +
+  "! Today is " +
+  dayoftheweek +
+  ", a perfect day for" +
+  favouriteactivity;
+
+console.log(sentence);
